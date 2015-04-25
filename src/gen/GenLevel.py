6d1bc7ea45b2edecda0f7649a728fb75b83c7a7d
MegaMolis <T.Chmelevskij@gmail.com>
2015-04-26 00:18:28 +0100

    GenLevel creates realtionships in one pass with leel generation

--- src/gen/GenLevel.py
+++ src/gen/GenLevel.py
@@ -14,25 +14,8 @@ class GenLevel(object):
         self.depth = depth
         self.level = []
         self.create_rooms()
-        self.add_neighbours()
 
+    #generates levels and relationships
     def create_rooms(self):
-        self.level = [[Room([0, 0, 0, 0]) for x in range(self.map_width)] for y in range(self.map_height)]
-
-        # Level attributes
-        print "Level x: ", len(self.level[0])
-        print "Level y: ", len(self.level)
-
-    def add_neighbours(self):
-
-        self.level = [[[random.randint(0, 1) for neighbour in self.level[x][y].neighbours] for x in range(self.map_width)] for y in range(self.map_height)]
-
-        # Create connections for the center room
-        """
-        start_room = self.level[3][3]
-        self.counter += 1
-        print self.counter
-        for neighbour in range(len(start_room.neighbours)):
-            start_room.neighbours[neighbour] = random.randint(0, 1)
-        """
-
+        self.level = [[Room([random.randint(0, 1) for i in range(0, 4)])
+                       for x in range(self.map_width)] for y in range(self.map_height)]
