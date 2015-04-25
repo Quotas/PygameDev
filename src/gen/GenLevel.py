99d5df960050e4c1b59a3a02c082e89c9039c830
MegaMolis <T.Chmelevskij@gmail.com>
2015-04-25 02:14:04 +0100

    Rendered squares and start room connections, CPU is occupied by x.org instead of python interpreter

--- src/gen/GenLevel.py
+++ src/gen/GenLevel.py
@@ -6,6 +6,8 @@ pygame.init()
 
 
 class GenLevel(object):
+    counter = 0
+
     def __init__(self, map_width, map_height, depth):
         self.map_height = map_height
         self.map_width = map_width
@@ -16,15 +18,14 @@ class GenLevel(object):
 
     def create_rooms(self):
         self.level = [[(Room([0, 0, 0, 0])) for x in range(self.map_width)] for y in range(self.map_height)]
-        room_thing = self.level[25][15]
 
         # Level attributes
         print "Level x: ", len(self.level[0])
         print "Level y: ", len(self.level)
-        print "Room properties: ", room_thing.neighbours
 
     def add_neighbours(self):
-        start_room = self.level[25][15]
+        start_room = self.level[3][3]
+        self.counter += 1
+        print self.counter
         for neighbour in range(len(start_room.neighbours)):
             start_room.neighbours[neighbour] = random.randint(0, 1)
-        print "Start room neighbours: ", start_room.neighbours
--- src/gen/Render.py
+++ src/gen/Render.py
@@ -1,12 +1,57 @@
 import pygame
 from GenLevel import GenLevel
-from Screen import Screen
+
+# Constants
+TILE_SIZE = 100
+NUM_OF_ROWS = 5
+NUM_OF_COLUMNS = 5
+MAP_WIDTH = TILE_SIZE * NUM_OF_COLUMNS
+MAP_HEIGHT = TILE_SIZE * NUM_OF_ROWS
 
 
 class Render(object):
     def __init__(self):
-        self.Level = GenLevel(51, 31, 5)
+        self.Level = GenLevel(5, 5, 5)
+        self.game_window = pygame.display.set_mode((MAP_WIDTH, MAP_HEIGHT))
+        self.render()
 
+        print "Initializing Render object: ", (self.Level.level[3][3].neighbours)
 
+    def render(self):
+        start_room = self.Level.level[3][3].neighbours
+        for row in range(MAP_HEIGHT):
+            for column in range(MAP_WIDTH):
+
+                # Draw the rooms
+                pygame.draw.rect(self.game_window, (200, 200, 200),
+                                 (column * TILE_SIZE, row * TILE_SIZE,
+                                  TILE_SIZE, TILE_SIZE), 25)
+
+        # Draw paths
+        for i in range(len(start_room)):
+            def draw_path():
+                if start_room[i] == 1:
+                    print("Connection drawn")
+                    if i == 0:
+                        pygame.draw.rect(self.game_window, (222, 23, 23),
+                                         (TILE_SIZE * 2 + TILE_SIZE/2, TILE_SIZE * 2,
+                                         TILE_SIZE / 10, TILE_SIZE / 10))
+                    elif i == 1:
+                        pygame.draw.rect(self.game_window, (222, 23, 23),
+                                         (TILE_SIZE * 3, TILE_SIZE * 2 + TILE_SIZE * .5,
+                                         TILE_SIZE / 10, TILE_SIZE / 10))
+                    elif i == 2:
+                        pygame.draw.rect(self.game_window, (222, 23, 23),
+                                         (TILE_SIZE * 2, TILE_SIZE * 3,
+                                         TILE_SIZE / 10, TILE_SIZE / 10))
+                    elif i == 3:
+                        pygame.draw.rect(self.game_window, (222, 23, 23),
+                                         (TILE_SIZE * 2, TILE_SIZE * 2 + TILE_SIZE * .5,
+                                         TILE_SIZE / 10, TILE_SIZE / 10))
+
+                else:
+                    print("No Connection")
+
+            draw_path()
 
 
