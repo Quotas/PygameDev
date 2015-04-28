e28ce89002ba077e59dd5b6db25745fdfd0933c5
Seamus Salt <seamussalt@gmail.com>
2015-04-28 18:43:55 +0100

    tile rendering fully working

--- src/gen/Screen.py
+++ src/gen/Screen.py
@@ -19,13 +19,13 @@ class Screen(object):
         for y in range(0, 64):
             for x in range(0, 64):
                 if self.tile_map[x + y * 64] == 0:
-                    self.tile_map[x + y * 64] = self.sprite_sheet.GRASS_TILE00.astype(np.uint32)
+                    self.tile_map[x + y * 64] = self.sprite_sheet.GRASS_TILE00
                 elif self.tile_map[x + y * 64] == 1:
-                    self.tile_map[x + y * 64] = self.sprite_sheet.GRASS_TILE01.astype(np.uint32)
+                    self.tile_map[x + y * 64] = self.sprite_sheet.GRASS_TILE01
                 elif self.tile_map[x + y * 64] == 2:
-                    self.tile_map[x + y * 64] = self.sprite_sheet.GRASS_TILE02.astype(np.uint32)
+                    self.tile_map[x + y * 64] = self.sprite_sheet.GRASS_TILE02
                 else:
-                    self.tile_map[x + y * 64] = self.sprite_sheet.GRASS_TILE03.astype(np.uint32)
+                    self.tile_map[x + y * 64] = self.sprite_sheet.GRASS_TILE03
 
 
     def create_tile_map(self):
@@ -35,11 +35,6 @@ class Screen(object):
                 xx = x
                 tileIndex = ((xx >> 5) & 63) + ((yy >> 5) & 63) * 64
                 self.pixels[x, y] = self.tile_map[tileIndex][(x & 31) + (y & 31) * 32].astype(np.uint32)
-
-    def render_tile(self, tile):
-        for y in range(0, len(tile)):
-            for x in range(0, len(tile)):
-                self.pixels[x, y] = tile.pixels[x, y]
 
     def update_tile_map(self, offsetX, offsetY):
         pass
--- src/gen/SpriteSheet.py
+++ src/gen/SpriteSheet.py
@@ -7,20 +7,21 @@ class SpriteSheet(object):
 
     def __init__(self):
         self.spritesheet = pygame.image.load("L:/Users/Seamus/Desktop/PygameDev/PygameDev/assets/sprites/spritesheet.png")
-        self.sprite_sheet_image_array = pygame.surfarray.array2d(self.spritesheet)
+        self.sprite_sheet_image_array = pygame.surfarray.array2d(self.spritesheet).astype(np.uint32)
         self.sprites = pygame.sprite.Group()
         self.GRASS_TILE00 = self.create_sprite(0, 0, 32)
         self.GRASS_TILE01 = self.create_sprite(32, 0, 32)
         self.GRASS_TILE02 = self.create_sprite(64, 0, 32)
         self.GRASS_TILE03 = self.create_sprite(0, 32, 32)
         self.STONE_TILE00 = self.create_sprite(96, 0, 32)
+        self.LAVA_TILE00 = self.create_sprite(32, 32, 32)
 
 
     def create_sprite(self, posx, posy, size):
         pixels = np.zeros(size * size)
         for x in range(0, size):
             for y in range(0, size):
-                pixels[x + y * size] = self.sprite_sheet_image_array[x + posx, y + posy]
+                pixels[x + y * size] = (self.sprite_sheet_image_array[x + posx][y + posy] & 0x0000FF) << 16 | (self.sprite_sheet_image_array[x + posx][y + posy] & 0x00FF00) | (self.sprite_sheet_image_array[x + posx][y + posy] & 0xFF0000) >> 16
         return pixels
 
     def render_sprite(self, xOffset, yOffset):
