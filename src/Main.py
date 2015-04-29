5e96eed1a806cf8cf150b7479b87d55731aaba41
Seamus Salt <seamussalt@gmail.com>
2015-04-29 01:35:41 +0100

    tile gen fully fixed
    can randomise level with r though slight lag going to try and find a fix

--- src/Main.py
+++ src/Main.py
@@ -15,20 +15,26 @@ def main():
     time_delta = 0
 
     while True:
+        keys = pygame.key.get_pressed()
         previous_time, current_time = current_time, time.clock()
         time_delta += (current_time - previous_time)
         scene.clear()
-        scene.render()
         frames += 1
-        #clock.tick(60)
+        clock.tick(60)
+
+        if keys[pygame.K_r]:
+            scene.pixel_data_buffer.randomise_tiles()
+            scene.pixel_data_buffer.create_tile_map()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+
         if time_delta >= 1:
             time_delta -= 1
             pygame.display.set_caption("fps:" + str(frames))
             frames = 0
-
+        scene.render()
 
 main()
--- src/gen/Screen.py
+++ src/gen/Screen.py
@@ -1,5 +1,6 @@
 import numpy as np
 import random
+import pygame
 
 from src.gen.SpriteSheet import SpriteSheet
 
@@ -10,31 +11,34 @@ class Screen(object):
         self.width = width
         self.height = height
         self.pixels = np.zeros((width, height)).astype(np.uint32)
-        self.tile_map = [0 for x in range(64*64)]
-
-        for y in range(0, 64):
-            for x in range(0, 64):
-                self.tile_map[x + y * 64] = random.randrange(0, 3)
-
-        for y in range(0, 64):
-            for x in range(0, 64):
-                if self.tile_map[x + y * 64] == 0:
-                    self.tile_map[x + y * 64] = self.sprite_sheet.GRASS_TILE00
-                elif self.tile_map[x + y * 64] == 1:
-                    self.tile_map[x + y * 64] = self.sprite_sheet.GRASS_TILE01
-                elif self.tile_map[x + y * 64] == 2:
-                    self.tile_map[x + y * 64] = self.sprite_sheet.GRASS_TILE02
-                else:
-                    self.tile_map[x + y * 64] = self.sprite_sheet.GRASS_TILE03
-
+        self.tile_map_width = 64
+        self.tile_map_height = 64
+        self.tile_map = [0 for x in range(64 * 64)]
+        self.randomise_tiles()
 
     def create_tile_map(self):
         for y in range(0, self.height):
             yy = y
             for x in range(0, self.width):
                 xx = x
-                tileIndex = ((xx >> 5) & 63) + ((yy >> 5) & 63) * 64
+                tileIndex = ((xx >> 5) & (64 - 1)) + ((yy >> 5) & (64 - 1)) * 64
                 self.pixels[x, y] = self.tile_map[tileIndex][(x & 31) + (y & 31) * 32].astype(np.uint32)
+
+    def randomise_tiles(self):
+        for y in range(0, 64):
+            for x in range(0, 64):
+                self.tile_map[x + y * 64] = random.randrange(0, 3)
+
+        for y in range(0, self.tile_map_height):
+            for x in range(0, self.tile_map_width):
+                if self.tile_map[x + y * self.tile_map_width] == 0:
+                    self.tile_map[x + y * self.tile_map_width] = self.sprite_sheet.GRASS_TILE00
+                elif self.tile_map[x + y * self.tile_map_width] == 1:
+                    self.tile_map[x + y * self.tile_map_width] = self.sprite_sheet.GRASS_TILE01
+                elif self.tile_map[x + y * self.tile_map_width] == 2:
+                    self.tile_map[x + y * self.tile_map_width] = self.sprite_sheet.BUSH_TILE00
+                elif self.tile_map[x + y * self.tile_map_width] == 5:
+                    self.tile_map[x + y * self.tile_map_width] = self.sprite_sheet.LAVA_TILE00
 
     def update_tile_map(self, offsetX, offsetY):
         pass
